72c54f7b6758190708c96fc83aacdade9889ea59
VinPhong09 <dongphongvin0909@gmail.com>
2025-03-25 22:11:50 +0700

    + add plugins: Addressable

---
+++ LegendOfChicken/Assets/Scripts/SceneLoader/SceneLoader.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField]private AssetReference addressableSceneName; // Tên Addressables của Scene
+    [SerializeField] private AsyncOperationHandle<SceneInstance> _handle;
+    public Slider loadingBar; // Gán Slider từ UI vào đây
+    public TextMeshProUGUI progressText;
+    
+        public void Start()
+        {
+            Caching.ClearCache();
+            LoadAddresableScene(addressableSceneName);
+        }
+
+        private void Update()
+        {
+            if (_handle.IsValid())
+            {
+                float currentProgress = _handle.GetDownloadStatus().Percent;
+                loadingBar.value = currentProgress;
+                progressText.text = currentProgress * 100 + "%";
+                Debug.Log(currentProgress);
+            }
+
+            if (Mathf.Approximately(_handle.PercentComplete, 1f))
+            {
+                Addressables.Release(_handle);
+            }
+        }
+
+        private async void LoadAddresableScene(AssetReference addressableSceneName)
+        {
+            _handle = Addressables.LoadSceneAsync(addressableSceneName, LoadSceneMode.Single);
+            await _handle.Task;
+        }
+}
